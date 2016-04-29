1fba8eb17f3f840206ebfd430fdcdcf05f50c6b4
Afaan <afaan.naqvi@gmail.com>
2016-04-29 14:47:10 -0700

    Update edit route and form - editing profiles allowed

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -58,12 +58,24 @@ end
 
 get '/users/:id/edit' do
   if current_user
+    @user = current_user
     erb :'/users/edit'
   else
     redirect 'logout'
   end
 
 end
+
+put '/users/:id' do
+  if current_user
+    user = current_user
+    user.update_attributes(user_name: params[:user_name], email: params[:email], password: params[:password])
+    erb :'/users/show'
+  else
+    redirect '/login'
+  end
+end
+
 
 # ______________________________________________________
 
